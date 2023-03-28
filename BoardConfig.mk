89f8f573354545914f663b7f6efdea8f938f3685
saku-bruh <notwolfyy@gmail.com>
2023-03-28 23:26:10 +0300

    pdx215: Enable 64-byte slab cache
    Signed-off-by: saku-bruh <notwolfyy@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,6 +64,7 @@ BOARD_KERNEL_CMDLINE := \
     androidboot.memcg=1 \
     kpti=off \
     lpm_levels.sleep_disabled=1 \
+    android_kmalloc_64_create=true \
     msm_rtb.filter=0x237 \
     service_locator.enable=1 \
     androidboot.usbcontroller=a600000.dwc3 \
