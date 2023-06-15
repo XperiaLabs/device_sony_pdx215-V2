88e2f0df4068aa800f54dbdeb5acace3e59e72b0
Omkar Chandorkar <gotenksIN@aospa.co>
2023-06-15 14:26:05 +0900

    pdx215: Setup SecureNFC
    Also set SKU prop as NFCService checks it, bootloader doesnt seem to set it even on stock
    
    [Saku]:
    Also dropped an unused NFC prop
    
    Change-Id: Ie4a08a7e93cab9bd0e6761ebc4aca0d20ff2b62e

--- device.mk
+++ device.mk
@@ -57,7 +57,8 @@ PRODUCT_PACKAGES += \
     SonySagamiSettingsResCommon \
     SonySagamiSystemUIResCommon \
     SonySagamiTelephonyResCommon \
-    WifiResCommon
+    WifiResCommon \
+    NfcResTarget
 
 # RRO Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
