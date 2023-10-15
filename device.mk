f6667e9441279d7c5cff5e9ca6bdb95d22898dd3
YuKongA <70465933+YuKongA@users.noreply.github.com>
2023-10-15 10:28:28 +0800

    pdx215: Update Power 1.2 to 1.3

--- device.mk
+++ device.mk
@@ -410,7 +410,7 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power@1.2.vendor \
+    android.hardware.power@1.3.vendor \
     vendor.qti.hardware.perf@2.2.vendor \
     libgrpc++_unsecure.vendor
 
