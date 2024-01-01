e616bfeb819ffbf8d910b6c03c0ed0cac22f74e2
saku-bruh <saku-bruh@proton.me>
2024-01-02 00:16:59 +0200

    pdx215: Re-introduce Boost Framework [1/2]
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- device.mk
+++ device.mk
@@ -432,8 +432,24 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power@1.3.vendor \
-    vendor.qti.hardware.perf@2.2.vendor \
     libgrpc++_unsecure.vendor
+
+# Perf Boost FW
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0 \
+    vendor.qti.hardware.perf@2.2 \
+    vendor.qti.hardware.perf@2.2.vendor \
+    vendor.qti.hardware.perf@2.3 \
+    libpsi.vendor \
+    libtflite \
+    vendor.qti.hardware.servicetracker@1.2.vendor \
+    vendor.qti.memory.pasrmanager@1.0.vendor
+
+# Perf Boot Jars
+PRODUCT_BOOT_JARS += \
+    QPerformance \
+    QXPerformance \
+    UxPerformance
 
 # Protobuf
 PRODUCT_PACKAGES += \
