922d0cc13379fff337c0c225f3da133ce40ea02e
kubersharma001 <kubersharma001@gmail.com>
2024-01-21 12:59:23 +0600

    pdx215: Compile libldacBT_bco.so
    * Fixes:
    I a2dp_vendor_ldac_decoder: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac_decoder.cc:123 - A2DP_VendorLoadDecoderLdac: A2DP_VendorLoadDecoderLdac: cannot open LDAC decoder library libldacBT_bco.so: dlopen failed: library "libldacBT_bco.so" not found
    E a2dp_vendor_ldac: packages/modules/Bluetooth/system/stack/a2dp/a2dp_vendor_ldac.cc:1414 - init: init: cannot load the decoder
    
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- device.mk
+++ device.mk
@@ -215,7 +215,8 @@ PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0.vendor \
     vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
-    vendor.qti.hardware.btconfigstore@2.0.vendor
+    vendor.qti.hardware.btconfigstore@2.0.vendor \
+    libldacBT_bco
 
 # Boot Control
 PRODUCT_PACKAGES += \
