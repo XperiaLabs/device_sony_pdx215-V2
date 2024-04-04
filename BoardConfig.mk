059ed20405d8a7729b04a9961d586f84bfefa1e9
Arindam Bhattacharjee <abhattacharjee717@gmail.com>
2024-04-04 22:41:19 +0530

    pdx215: Implement Codec2 support [1/2]
    Signed-off-by: Arindam Bhattacharjee <abhattacharjee717@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -185,8 +185,11 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(DEVICE_PATH)/configs/vintf/framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     vendor/lineage/config/device_framework_matrix.xml
+TARGET_USES_CUSTOM_C2_MANIFEST := true
 DEVICE_MATRIX_FILE += $(DEVICE_PATH)/configs/vintf/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/vintf/manifest.xml
+DEVICE_MANIFEST_FILE += \
+    $(DEVICE_PATH)/configs/vintf/manifest.xml \
+    $(DEVICE_PATH)/configs/vintf/c2_manifest_vendor.xml
 
 # Init Script
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_pdx215
