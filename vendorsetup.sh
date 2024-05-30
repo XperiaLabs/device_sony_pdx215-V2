b11802592a83c37922bce63cc2f28f44df7685cd
ahnet-69 <syedmusicrealise@gmail.com>
2024-05-30 18:37:25 +0500

    pdx215: configs: patches: Add X-Reality Engine Patches
    * Also cleaned up by moving the patches in their own sub-dirs

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,12 +3,19 @@
 echo -e
 echo 'Start Applying needed patches'
 echo -e
-echo 'Applying Dolby patch in frameworks/av'
+echo 'Applying VTService patch in frameworks/av'
 echo -e
 cd frameworks/av
 git reset --hard FETCH_HEAD
 echo -e
-git am ../../device/sony/pdx215/configs/patches/frameworks_av/0001-Fixes-vtservice-cpu-hogging.patch
+git am ../../device/sony/pdx215/configs/patches/frameworks_av/VTService/*
+cd ../..
+echo -e
+echo 'Applying X-Reality Engine patches in frameworks/base'
+cd frameworks/base
+git reset --hard FETCH_HEAD
+echo -e
+git am ../../device/sony/pdx215/configs/patches/frameworks_base/X-Reality/*
 cd ../..
 echo -e
 echo 'Done applying'
