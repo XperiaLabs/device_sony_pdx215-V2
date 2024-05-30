c077b3e2c51f3dc0e5ae07bcd2a75592cce5f9b9
saku-bruh <saku-bruh@proton.me>
2024-05-30 14:10:37 +0300

    pdx215: Drop duplicate flag
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- device.mk
+++ device.mk
@@ -269,9 +269,7 @@ PRODUCT_VENDOR_PROPERTIES += \
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := everything
 
-PRODUCT_SYSTEM_PROPERTIES += \
-    dalvik.vm.dex2oat-filter=everything \
-    dalvik.vm.image-dex2oat-filter=everything
+PRODUCT_SYSTEM_PROPERTIES += dalvik.vm.dex2oat-filter=everything
 
 # Display
 PRODUCT_PACKAGES += \
