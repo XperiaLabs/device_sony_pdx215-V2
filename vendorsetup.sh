c1ae951f887b73aee20c40960c04c36f3335666d
ahnet-69 <syedmusicrealise@gmail.com>
2024-07-06 18:08:48 +0000

    pdx215: configs: patches: Add patch for AC4 Decoder

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo -e
-echo 'Start Applying needed patches'
+echo 'Started applying needed patches...'
 echo -e
 echo 'Applying Dolby patch in frameworks/av'
 echo -e
@@ -11,6 +11,14 @@ echo -e
 git am ../../device/sony/pdx215/configs/patches/frameworks_av/0001-Fixes-vtservice-cpu-hogging.patch
 cd ../..
 echo -e
-echo 'Done applying'
+echo 'Done applying VTService patch!'
 echo -e
-echo 'Continue'
+echo 'Applying Dolby AC4 patch in frameworks/av'
+echo -e
+cd frameworks/av
+echo -e
+git am ../../device/sony/pdx215/configs/patches/frameworks_av/0002-Revert-OMX-Remove-support-for-prebuilt-ac4-decoder.patch
+cd ../..
+echo -e
+echo 'Done applying AC4 Decoder patch!'
+echo -e
