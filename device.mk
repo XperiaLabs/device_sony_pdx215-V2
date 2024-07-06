e7ae65d4c4b3cb48106c205e1bbaeb71a73437c5
saku-bruh <saku-bruh@proton.me>
2024-07-06 03:52:36 +0300

    pdx215: Re-implement XperiaCharger [1/2]
    Signed-off-by: saku-bruh <saku-bruh@proton.me>

--- device.mk
+++ device.mk
@@ -602,6 +602,7 @@ $(call inherit-product, vendor/sony/extra/Sagami/extra.mk)
 # Xperia Modules - Flags
 TARGET_SHIPS_XPERIA_SETTINGS_MENU := true
 TARGET_SUPPORTS_IMAGE_ENHANCEMENT := true
+TARGET_SUPPORTS_BATTERY_CARE := true
 TARGET_SUPPORTS_HIGH_REFRESH_RATE := true
 TARGET_SUPPORTS_HIGH_POLLING_RATE := true
 
